57c481ddf0fe9042cf989a74f98013dcca0e873b
Patrick dela Plana <patrick.delaplana@gmail.com>
2025-06-07 06:48:07 +0000

    Can we change the background color of the information boxes

--- src/components/ui/alert.tsx
+++ src/components/ui/alert.tsx
@@ -1,14 +1,15 @@
+
 import * as React from "react"
 import { cva, type VariantProps } from "class-variance-authority"
 
 import { cn } from "@/lib/utils"
 
 const alertVariants = cva(
-  "relative w-full rounded-lg border p-4 [&>svg~*]:pl-7 [&>svg+div]:translate-y-[-3px] [&>svg]:absolute [&>svg]:left-4 [&>svg]:top-4 [&>svg]:text-foreground",
+  "relative w-full rounded-lg border p-4 [&>svg~*]:pl-7 [&>svg+div]:translate-y-[-3px] [&>svg]:absolute [&>svg]:left-4 [&>svg]:top-4",
   {
     variants: {
       variant: {
-        default: "bg-background text-foreground",
+        default: "border-[hsl(var(--accent)/0.3)] bg-[hsl(var(--accent)/0.1)] text-foreground [&>svg]:text-[hsl(var(--accent))]",
         destructive:
           "border-destructive/50 text-destructive dark:border-destructive [&>svg]:text-destructive",
       },
